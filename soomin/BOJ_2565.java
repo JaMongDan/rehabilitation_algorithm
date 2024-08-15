b9d8e16c5a64c26f25b3359421836eb0d59602ec
tnatna0801 <min120926@gmail.com>
2024-08-16 02:06:14 +0900

    [BOJ]2565. 전깃줄
    이분탐색(성공)

--- soomin/BOJ_2565.java
+++ soomin/BOJ_2565.java
@@ -55,35 +55,48 @@ public class BOJ_2565 {
         Collections.sort(wireList);
 
         /*
-         * 0~i-1번째 전봇대들을 탐색하면서 i번째 전봇대를 B와 연결해도 되는 지 탐색하는 메소드
-         * i번째 전봇대에 연결된 도착 전봇대(B)는 0~i-1번째 전봇대에 연결된 B보다 커야 안켭침
+         * 이분탐색을 사용하여 현재 i번째 전봇대와 연결되는 b 전봇대가 dp 테이블의 어떤 위치에 포함될지 탐색
+         * 탐색 범위는 0 ~ size(dp 배열의 유효한 길이)입니당
          */
         int size = 0; // dp 배열의 의미있는 길이
         for (int i = 0; i < N; i++) {
 
             int b = wireList.get(i).b;
+
+            // 종료값이 size인 이유는 이분탐색의 반복 조건이 left < right이기 때문입니다.
+            // left == right면 종료되기 때문에 right를 size-1(인덱스)로 두면 마지막 원소를 탐색 못합니다.
+            // 따라서 size는 사실상 최종인덱스 + 1이고 size를 right으로 두어야 마지막 원소까지 탐색이 가능합니다.
             int pos = binarySearch(0, size, b);
 
-            if (pos < 0) pos = -pos - 1;
             dp[pos] = b;
-
-            if(pos == size) size++; // dp 배열의 길이를 추가!
+            if(pos == size) size++; // b값이 추가되었기 때문에 증가!
 
         }
 
         System.out.println(N - size); // 철거한 갯수를 출력
     }
 
+    /**
+     * b 전봇대가 dp 배열의 어느 위치에 위치할 수 있을지 탐색하는 이분탐색 메소드입니다.
+     * b 전봇대의 값과 db 배열의 원소값을 비교하면서
+     * 더 크다면 위치를 오른쪽으로 이동하고
+     * 작다면 왼쪽으로 이동합니당
+     *
+     * @param left 탐색 시작 인덱스 0으로 고정입니다.
+     * @param right 탐색 종료 인덱스 dp 배열의 유효 길이입니다
+     * @param value 현재 탐색할 a전봇대가 연결될 b 전봇대 값입니당
+     * @return
+     */
     private static int binarySearch(int left, int right, int value) {
 
 
         while (left < right) {
-            int mid =  (left + right) / 2;
+            int mid =  (left + right) / 2; // 삽입할 위치
 
             if (dp[mid] < value) left = mid + 1;
             else right = mid;
         }
 
-        return left - 1;
+        return right; // 삽입할 위치를 반환
     }
 }
