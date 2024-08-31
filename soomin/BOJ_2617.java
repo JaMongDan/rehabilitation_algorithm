9a1b795acb52b2d4f5ecfb8738fd07ebb573954c
tnatna0801 <min120926@gmail.com>
2024-09-01 00:54:28 +0900

    [BOJ]2617. 구슬찾기
    플로이드 워셜

--- soomin/BOJ_2617.java
+++ soomin/BOJ_2617.java
@@ -13,7 +13,7 @@ public class BOJ_2617 {
         int M = Integer.parseInt(st.nextToken());
 
         // 1. 입력: 인접행렬
-        // bead[b][a] = 1이면, 구슬 b가 구슬 a보다 큰 것을 의미
+        // bead[b][a] = 1이면, 구슬 a가 구슬 b보다 큰 것을 의미
         // bead 배열을 boolean으로도 해도 되는데 int로 한 이유는 각 원소가 몇개의 원소보다 큰지 작은지 탐색할 때 if문을 사용하지 않고 그냥 더해서 계산할라구 int 배열 썼습니다.
         int[][] bead = new int[N + 1][N + 1];
         int limit = (N + 1) / 2;
@@ -23,7 +23,7 @@ public class BOJ_2617 {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            bead[b][a] = 1;
+            bead[b][a] = 1; // a -> b 연결 ( 작은쪽에서 큰쪽으로 )
         }
 
         // 2. 플로이드-워셜을 사용해 모든 쌍의 최당 경로 여부를 계산
